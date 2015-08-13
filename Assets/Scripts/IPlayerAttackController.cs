569890767d6685653f3e22484f9c6940de7a5e8d
danielhollcraft <daniel@hollcraft.com>
2015-08-13 18:09:44 +0200

    removes files.

--- Assets/Scripts/IPlayerAttackController.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-// TODO: Merge with EnemyAttackController somehow.
-public interface IPlayerAttackController {
-	void Fire(Vector3 direction);
-	Vector3 GetFireAimAxes();
-}
